af48b074a73ba1f7a22de2380ea29a0397063135
seyyedAlirezaGhazanfari <59168138+seyyedAlirezaGhazanfari@users.noreply.github.com>
2022-03-13 22:10:59 +0330

    fix(login): change format

--- app/src/main/java/com/example/ourcw/MainActivity.java
+++ app/src/main/java/com/example/ourcw/MainActivity.java
@@ -1,36 +1,47 @@
 package com.example.ourcw;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
-import androidx.navigation.ui.AppBarConfiguration;
-import androidx.navigation.ui.NavigationUI;
-
-import com.example.ourcw.databinding.ActivityMainBinding;
-import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
-
-    private ActivityMainBinding binding;
-
+    private Button navToRegistrationStudentButton;
+    private Button navToRegistrationTeacherButton;
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        binding = ActivityMainBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
-
-        BottomNavigationView navView = findViewById(R.id.nav_view);
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.navigation_teacher, R.id.navigation_student)
-                .build();
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_login);
-        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
-        NavigationUI.setupWithNavController(binding.navView, navController);
+        setContentView(R.layout.activity_main);
+        navToRegistrationStudentButton = findViewById(R.id.navToRegistrationStudentButtonId);
+        navToRegistrationTeacherButton = findViewById(R.id.navToRegistrationTeacherButtonId);
+        navToRegistrationTeacherButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(
+                        MainActivity.this,
+                        TeacherRegistrationActivity.class
+                );
+                startActivityForResult(
+                        intent, 100
+                );
+            }
+        });
+        navToRegistrationStudentButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(
+                        MainActivity.this,
+                        StudentRegistrationActivity.class
+                );
+                startActivityForResult(
+                        intent, 100
+                );
+            }
+        });
     }
+
 
 }
---
+++ app/src/main/java/com/example/ourcw/TeacherRegistrationActivity.java
@@ -0,0 +1,28 @@
+package com.example.ourcw;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class TeacherRegistrationActivity extends AppCompatActivity {
+
+    private Button backButton;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_teacher_registration);
+        Intent intent = getIntent();
+        backButton = findViewById(R.id.teacherBackLoginButtonId);
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent1 = new Intent();
+                setResult(100, intent1);
+                finish();
+            }
+        });
+    }
+}
